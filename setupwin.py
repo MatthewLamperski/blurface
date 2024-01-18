cf57e32cef2eea0c06a23c5255cce4ec0720525c
Matthew Lamperski <matthew.lamperski@gmail.com>
2024-01-17 22:48:38 -0500

    setupwin

---
+++ setupwin.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+import py2exe
+import os
+import sys
+
+# Find the directory where the Haar cascade files are located
+haar_cascade_directory = os.path.dirname('.')
+haar_cascade_files = [os.path.join(haar_cascade_directory, 'haarcascade_frontalface_default.xml'),
+                      os.path.join(haar_cascade_directory, 'haarcascade_profileface.xml')]
+
+# Define the data_files to include the Haar cascade files
+data_files = [('data', haar_cascade_files)]
+
+# Define options for py2exe
+options = {
+    'py2exe': {
+        'bundle_files': 1,  # Bundle everything into a single file
+        'compressed': True, # Compress the library archive
+        'includes': ['sip', 'PyQt5.QtWidgets', 'cv2'], # Include these modules
+    }
+}
+
+# Setup configuration
+setup(
+    name='BlurFace',
+    version='1.0',
+    description='Record a video with all faces blurred!',
+    author='Matthew Lamperski',
+    windows=[{'script': 'video_capture_widget.py'}],  # Replace with your main script
+    options=options,
+    zipfile=None,  # Include everything in the exe, instead of a zipfile
+    data_files=data_files
+)
